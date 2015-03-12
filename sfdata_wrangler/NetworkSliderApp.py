9c979a659a64250ee044ebc0855f260b6f2532c9
Gregory D. Erhardt <greg.erhardt.13@ucl.ac.uk>
2015-03-12 11:53:54 +0000

    Rename slider_app.py to NetworkSliderApp.py

--- sfdata_wrangler/NetworkSliderApp.py
+++ sfdata_wrangler/NetworkSliderApp.py
@@ -1,112 +1,49 @@
-__author__      = "Gregory D. Erhardt"
-__copyright__   = "Copyright 2013 SFCTA"
-__license__     = """
-    This file is part of sfdata_wrangler.
-
-    sfdata_wrangler is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    sfdata_wrangler is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with sfdata_wrangler.  If not, see <http://www.gnu.org/licenses/>.
 """
+This file demonstrates a bokeh applet, which can be viewed directly
+on a bokeh-server. See the README.md file in this directory for
+instructions on running.
+"""
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 import numpy as np
 import pandas as pd
 
 from bokeh.plotting import figure
-from bokeh.models import Plot, ColumnDataSource
+from bokeh.models import Plot, ColumnDataSource, MultiLine
 from bokeh.properties import Instance
 from bokeh.server.app import bokeh_app
 from bokeh.server.utils.plugins import object_page
-from bokeh.models.widgets import HBox, Slider, VBoxForm
+from bokeh.models.widgets import VBox, Slider, VBoxForm
 
 import Visualizer
 from HwyNetwork import HwyNetwork
 
+# global parameters
 INPUT_DYNAMEQ_NET_DIR    = "C:/CASA/Data/network/dynameq/validation2010.july19_Sig/Reports/Export"
 INPUT_DYNAMEQ_NET_PREFIX = "pb_july19_830p"
 TAXI_OUTFILE = "C:/CASA/DataExploration/taxi.h5"     
 LOGGING_DIR = "C:/CASA/DataExploration"
 
+class NetworkSliderApp(VBox):
+    """An example of a browser-based, interactive plot with slider controls."""
 
-
-def prepareLinkData(self, hwynet, date='2013-02-13'):
-    """ 
-    Returns a column data source with one record for each link, containing 
-    the data necessary for plotting. 
-    
-    date - string for the date's data to display
-    """
-        
-    # read the highway network
-    hwynet = HwyNetwork()
-    hwynet.readDTANetwork(INPUT_DYNAMEQ_NET_DIR, INPUT_DYNAMEQ_NET_PREFIX, logging_dir=LOGGING_DIR) 
-
-    # start with the network links as a dataframe
-    df = hwynet.getRoadLinkDataFrame()
-        
-    # now get the link speeds for the date, and for each hour
-    store = pd.HDFStore(TAXI_OUTFILE)
-    for hour in range(0,24):
-        h = str(hour)
-            
-        # get the data
-        query = "date==Timestamp('" + date + "') and hour==" + h
-        obs_df = store.select('link_tt', where=query) 
-
-        # append the hour to the end of each column name for this query
-        obs_df.drop('date', axis=1, inplace=True)
-        obs_df.rename(columns=lambda x: x+h, inplace=True)
-
-        # merge, keeping all links
-        df = pd.merge(df, obs_df, how='left', 
-                      left_on=['ID'], right_on=['link_id'+h])
-                        
-        """ Calculations start here """
-        # there are zero observations if its not in the righthand database
-        df['observations'+h].replace(to_replace=np.nan, value=0, inplace=True)
-            
-        # calculate some extra fields
-        length_tt_fftt = pd.Series(zip(df['LENGTH'], df['tt_mean'+h], df['FFTIME']))
-        df['speed'+h] = length_tt_fftt.apply(Visualizer.calculateSpeed)
-            
-        tt_fftt = pd.Series(zip(df['tt_mean'+h], df['FFTIME']))
-        df['tt_ratio'+h] = tt_fftt.apply(Visualizer.calculateTravelTimeRatio)
-            
-        # map the link colors based on the travel time ratio
-        df['color'+h] = df['tt_ratio'+h].apply(Visualizer.getLinkTTRatioColor)
-                
-    store.close()
-
-    df['color'] = df['color0']
-
-    source = ColumnDataSource(df)
-    return source
-        
-
-class NetworkSliderApp(HBox):
-    """A browser-based, interactive plot with slider controls.
-    For exploring network attributes. 
-    """
-
-    extra_generated_classes = [["NetworkSliderApp", "NetworkSliderApp", "HBox"]]
+    extra_generated_classes = [["NetworkSliderApp", "NetworkSliderApp", "VBox"]]
 
     inputs = Instance(VBoxForm)
 
     hour = Instance(Slider)
+    
     plot = Instance(Plot)
 
-    source = Instance(ColumnDataSource)
+    allLinkData = Instance(ColumnDataSource)
+    selectedLinkData = Instance(ColumnDataSource)
+
 
     @classmethod
-    def create(cls, hwynet):
+    def create(cls):
         """One-time creation of app's objects.
 
         This function is called once, and is responsible for
@@ -114,11 +51,25 @@ class NetworkSliderApp(HBox):
         """
         obj = cls()
 
-        obj.source = obj.prepareLinkData(hwynet)
+        obj.allLinkData = ColumnDataSource(data=dict(X=[], 
+                                                          Y=[], 
+                                                          LANES=[], 
+                                                          FFSPEED=[],
+                                                          observations=[],
+                                                          speed=[],
+                                                          color=[]))
+        
+        obj.selectedLinkData = ColumnDataSource(data=dict(X=[], 
+                                                          Y=[], 
+                                                          LANES=[], 
+                                                          FFSPEED=[],
+                                                          observations=[],
+                                                          speed=[],
+                                                          color=[]))
         
         obj.hour = Slider(
             title="Time of Day", name="hour",
-            value=0, start=0, end=24, step=1
+            value=0, start=0, end=23, step=1
         )
 
         # Generate a figure container
@@ -130,24 +81,23 @@ class NetworkSliderApp(HBox):
                       y_axis_type=None,
                       tools="pan,wheel_zoom,reset,hover,save", 
                       title="SF Taxi Speeds") 
-        
+                    
         # plot the links
         plot.multi_line(xs='X', 
                         ys='Y', 
                         line_width='LANES',  
                         line_color='color', 
-                        source=obj.source)    
-
+                        source=obj.selectedLinkData)    
+                        
         obj.plot = plot
         obj.update_data()
 
         obj.inputs = VBoxForm(children=[obj.hour])
 
-        obj.children.append(obj.inputs)
         obj.children.append(obj.plot)
-
+        obj.children.append(obj.inputs)
+        
         return obj
-
 
     def setup_events(self):
         """Attaches the on_change event to the value property of the widget.
@@ -161,7 +111,7 @@ class NetworkSliderApp(HBox):
         # Slider event registration
         for w in ["hour"]:
             getattr(self, w).on_change('value', self, 'input_change')
-
+            
 
     def input_change(self, obj, attrname, old, new):
         """Executes whenever the input form changes.
@@ -175,20 +125,92 @@ class NetworkSliderApp(HBox):
             new : new value of attr
         """
         self.update_data()
-
+        
 
     def update_data(self):
         """Called each time that any watched property changes.
 
-        Update the data with the details columns specific to this
-        hour. 
+        select the appropriate columns for this hour
         """
-        # Get the current slider values
+        if len(self.allLinkData.data['X']) == 0:
+            self.allLinkData.data = self.prepareLinkData()
+            #self.selectedLinkData.data = dict(X=self.allLinkData.data['X'], 
+            #                              Y=self.allLinkData.data['Y'], 
+            #                              LANES=self.allLinkData.data['LANES'], 
+            #                              color=self.allLinkData.data['color'])
+        
         h = str(self.hour.value)
+        colorString = 'color' + h
+        self.selectedLinkData.data = dict(X=self.allLinkData.data['X'], 
+                                          Y=self.allLinkData.data['Y'], 
+                                          LANES=self.allLinkData.data['LANES'], 
+                                          color=self.allLinkData.data[colorString])
 
-        self.source.data['color'] = self.source.data['color'+h]
+        self.plot.title = "SF Taxi Speeds for Hour: " + h + ":00"
 
+        print 'updating to hour ' + h
+            
 
+    def prepareLinkData(self, date='2009-02-13'):
+        """ 
+        Reads and returns a dictionary with one record for each link, containing 
+        the data necessary for plotting. 
+        
+        Called once at the beginning to read in all the data. 
+        
+        date - string for the date's data to display
+        """
+        
+        # read the highway network
+        hwynet = HwyNetwork()
+        hwynet.readDTANetwork(INPUT_DYNAMEQ_NET_DIR, INPUT_DYNAMEQ_NET_PREFIX, logging_dir=LOGGING_DIR) 
+    
+        # start with the network links as a dataframe
+        df = hwynet.getRoadLinkDataFrame()
+        
+        # now get the link speeds for the date, and for each hour
+        store = pd.HDFStore(TAXI_OUTFILE)
+    
+        for hour in range(0,24):
+            h = str(hour)
+                
+            # get the data
+            query = "date==Timestamp('" + date + "') and hour==" + h
+            obs_df = store.select('link_tt', where=query) 
+    
+            # append the hour to the end of each column name for this query
+            obs_df.drop('date', axis=1, inplace=True)
+            obs_df.rename(columns=lambda x: x+h, inplace=True)
+    
+            # merge, keeping all links
+            df = pd.merge(df, obs_df, how='left', 
+                        left_on=['ID'], right_on=['link_id'+h])
+                            
+            """ Calculations start here """
+            # there are zero observations if its not in the righthand database
+            df['observations'+h].replace(to_replace=np.nan, value=0, inplace=True)
+            
+            # calculate some extra fields
+            length_tt_fftt = pd.Series(zip(df['LENGTH'], df['tt_mean'+h], df['FFTIME']))
+            df['speed'+h] = length_tt_fftt.apply(Visualizer.calculateSpeed)
+                
+            tt_fftt = pd.Series(zip(df['tt_mean'+h], df['FFTIME']))
+            df['tt_ratio'+h] = tt_fftt.apply(Visualizer.calculateTravelTimeRatio)
+                
+            # map the link colors based on the travel time ratio
+            df['color'+h] = df['tt_ratio'+h].apply(Visualizer.getLinkTTRatioColor)
+                                
+        store.close()
+    
+        df['color'] = df['color0']  
+
+        # convert to a dictionary.  
+        # .to_dict() returns in a different structure that doesn't work. 
+        d = {}
+        for c in df.columns: 
+            d[c] = df[c]
+                
+        return d
 
 
 """
@@ -202,9 +224,8 @@ Now navigate to the following URL in a browser:
 
     http://localhost:5006/bokeh/sf
 """
-
 @bokeh_app.route("/bokeh/sf/")
 @object_page("NetworkSlider")
 def make_sliders():
-    app = NetworkSliderApp.create(hwynet)
+    app = NetworkSliderApp.create()
     return app
