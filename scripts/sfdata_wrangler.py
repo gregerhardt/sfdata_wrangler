f4ad5525198b5b08f78e8bca82786557b1dbd768
Gregory D. Erhardt <greg.erhardt.13@ucl.ac.uk>
2015-09-15 16:11:52 +0100

    Read LODES resident-area characteristic (RAC) file to get number of workers by home location.

--- scripts/sfdata_wrangler.py
+++ scripts/sfdata_wrangler.py
@@ -81,23 +81,23 @@ RAW_STP_FILES =["D:/Input/SFMTA Data/Raw STP Files/0803.stp",
 RAW_GTFS_FILES = [
   #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20090402_0310_1.zip",  # 20090221 to 20090612 (originally 20090626)
                                                                                            # above file modified to avoid overlap of 13 days
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20091106_0310_1.zip",  # 20090613 to 20091204   (removed trailing sapced from file)
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100415_0222.zip",  # 20091205 to 20100507
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100812_0223_1.zip",  # 20100508 to 20100903
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100908_0243_1.zip",  # 20100904 to 20110102 (originally 20101231)
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20091106_0310_1.zip",  # 20090613 to 20091204   (removed trailing sapced from file)
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100415_0222.zip",  # 20091205 to 20100507
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100812_0223_1.zip",  # 20100508 to 20100903
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20100908_0243_1.zip",  # 20100904 to 20110102 (originally 20101231)
                                                                                            # above file modified to avoid gap of 2 days
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110104_0839.zip",  # 20110103 to 20110121
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110420_0243_1.zip",  # 20110122 to 20110612 (originally 20110610)
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110104_0839.zip",  # 20110103 to 20110121
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110420_0243_1.zip",  # 20110122 to 20110612 (originally 20110610)
                                                                                            # above file modified to avoid gap of 2 days
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110910_0449.zip",  # 20110613 to 20111014
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20111210_0410.zip",  # 20111015 to 20120120
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20120319_0337_1.zip",  # 20120121 to 20120608 (originally 20120615)
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20110910_0449.zip",  # 20110613 to 20111014
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20111210_0410.zip",  # 20111015 to 20120120
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20120319_0337_1.zip",  # 20120121 to 20120608 (originally 20120615)
                                                                                            # above file modified to avoid overlap of 6 days
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20120908_0325.zip",  # 20120609 to 20120928
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130302_0432_1.zip",  # 20120929 to 20130329 (originally 20130322)
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20120908_0325.zip",  # 20120609 to 20120928
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130302_0432_1.zip",  # 20120929 to 20130329 (originally 20130322)
                                                                                            # above file modified to avoid gap of 8 days
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130612_0307.zip",  # 20130330 to 20130628
-  #"D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130910_2349.zip",  # 20130629 to 20131025
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130612_0307.zip",  # 20130330 to 20130628
+  "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20130910_2349.zip",  # 20130629 to 20131025
   "D:/Input/GTFS/san-francisco-municipal-transportation-agency_20140117_0111.zip"   # 20131026 to 20140131  
   ]
 
@@ -124,6 +124,11 @@ RAW_CLIPPER_FILES =["D:/Input/Clipper/2013_-_3_Anonymous_Clipper.csv",
                    ]
 
 QCEW_DIR = "D:/Input/QCEW/"
+
+LODES_RAC_DIR = "D:/Input/Census/LEHD/LODES/CA/RAC/"
+LODES_WAC_DIR = "D:/Input/Census/LEHD/LODES/CA/WAC/"
+LODES_OD_DIR  = "D:/Input/Census/LEHD/LODES/CA/OD/"
+LODES_XWALK_FILE= "D:/Input/Census/LEHD/LODES/CA/ca_xwalk.csv"
 
 CENSUS_POPEST_PRE2010_FILE  = "D:/Input/Census/AnnualPopulationEstimates/2000to2010/CO-EST00INT-TOT.csv"
 CENSUS_POPEST_POST2010_FILE = "D:/Input/Census/AnnualPopulationEstimates/post2010/PEP_2014_PEPANNRES_with_ann.csv"
@@ -187,7 +192,7 @@ if __name__ == "__main__":
                                 daily_trip_outfile=DAILY_TRIP_OUTFILE, 
                                 daily_ts_outfile=DAILY_TS_OUTFILE, 
                                 dow=[1], 
-                                startDate='2013-11-01')
+                                startDate='2000-01-01')
         for infile in RAW_GTFS_FILES: 
             gtfsHelper.expandAndWeight(infile)   
         print 'Finished expanding to GTFS in ', (datetime.datetime.now() - startTime)
@@ -213,11 +218,16 @@ if __name__ == "__main__":
     if 'demandDrivers' in STEPS_TO_RUN: 
         startTime = datetime.datetime.now()   
         demandHelper = DemandHelper()
-        demandHelper.processQCEWData(QCEW_DIR, FIPS, DEMAND_OUTFILE)              
+        
+        demandHelper.processLODESRAC(LODES_RAC_DIR, LODES_XWALK_FILE, FIPS, DEMAND_OUTFILE) 
+        
+        demandHelper.processQCEWData(QCEW_DIR, FIPS, CPI_FILE, DEMAND_OUTFILE)  
+                    
         demandHelper.processCensusPopulationEstimates(CENSUS_POPEST_PRE2010_FILE, 
                                                       CENSUS_POPEST_POST2010_FILE, 
                                                       FIPS, 
-                                                      DEMAND_OUTFILE)             
+                                                      DEMAND_OUTFILE)      
+                                                             
         demandHelper.processFuelPriceData(FUEL_COST_FILE, CPI_FILE, DEMAND_OUTFILE)
         print 'Finished processing drivers of demand data ', (datetime.datetime.now() - startTime) 
         
--- sfdata_wrangler/DemandHelper.py
+++ sfdata_wrangler/DemandHelper.py
@@ -19,7 +19,9 @@ __license__     = """
 """
 
 import pandas as pd
+import numpy as np
 import glob
+import os
 
     
 class DemandHelper():
@@ -28,8 +30,10 @@ class DemandHelper():
     
     """
 
-    # the most recent year included in the census population estimates
-    MAX_YEAR = 2014
+    # the range of years for these data files
+    POP_EST_YEARS = [2000,2014]
+    LODES_YEARS   = [2002,2013]
+    
 
     def __init__(self):
         '''
@@ -37,7 +41,35 @@ class DemandHelper():
 
         '''   
     
-    def processQCEWData(self, inputDir, fips, outfile): 
+    def getCPIFactors(self, cpiFile):
+        """ 
+        Reads CPI numbers and returns a dataframe with the CPI_FACTOR field
+        that can be joined by month and used to adjust monetary values by 
+        inflation to 2010 US dollars.  
+        
+        """
+        
+        # get the CPI and convert to monthly format
+        dfcpi = pd.read_excel(cpiFile, sheetname='BLS Data Series', skiprows=10, index_col=0)
+        base = dfcpi.at[2010, 'Annual']
+        
+        dfcpi = dfcpi.drop(['Annual', 'HALF1', 'HALF2'], axis=1)
+        dfcpi = dfcpi.stack()
+        dfcpi = dfcpi.reset_index()
+        dfcpi = dfcpi.rename(columns={
+                             'level_1' : 'monthString', 
+                             0 : 'CPI'
+                             })
+                             
+        dfcpi['MONTH'] = '01-' + dfcpi['monthString'] + '-' + dfcpi['Year'].astype('string')
+        dfcpi['MONTH'] = dfcpi['MONTH'].apply(pd.Timestamp)
+        
+        dfcpi['CPI_FACTOR'] = base / dfcpi['CPI']
+        
+        return dfcpi
+
+    
+    def processQCEWData(self, inputDir, fips, cpiFile, outfile): 
         """ 
         Reads raw QCEW data and converts it to a clean list format. 
         
@@ -53,44 +85,86 @@ class DemandHelper():
         if '/countyEmp' in keys: 
             outstore.remove('countyEmp')
         
+        # create an empty dataframe with the right fields
+        dfout = pd.DataFrame()
+        
         # get the appropriate data
         pattern = inputDir + '*.q1-q4.by_area/*.q1-q4 ' + fips + '*.csv'
         infiles = glob.glob(pattern)
             
-        monthCount = 0
         for infile in infiles: 
             print 'Reading QCEW data in ' + infile
                 
-            dfin = pd.read_csv(infile)
-            dfin = dfin[(dfin['own_title']=='Total Covered') & (dfin['industry_title']=='Total, all industries')]
+            df_allrows = pd.read_csv(infile)
+            
+            # first get the average earnings for all industries
+            # own_code 0 is all ownership categories
+            dfin = df_allrows[(df_allrows['own_code']==0) & (df_allrows['industry_title']=='Total, all industries')]
             
             year = dfin['year'][0]
             months = pd.date_range(str(year-1) + '-12-31', periods=12, freq='M') + pd.DateOffset(days=1)
-                
-            dfout = pd.DataFrame({'MONTH': months})
-            dfout['TOTEMP'] = 0
 
-            # copy the data into straight file
-            dfout.at[0,'TOTEMP'] = dfin[dfin['qtr']==1]['month1_emplvl']   # jan
-            dfout.at[1,'TOTEMP'] = dfin[dfin['qtr']==1]['month2_emplvl']   # feb
-            dfout.at[2,'TOTEMP'] = dfin[dfin['qtr']==1]['month3_emplvl']   # mar
-            dfout.at[3,'TOTEMP'] = dfin[dfin['qtr']==2]['month1_emplvl']   # apr
-            dfout.at[4,'TOTEMP'] = dfin[dfin['qtr']==2]['month2_emplvl']   # may
-            dfout.at[5,'TOTEMP'] = dfin[dfin['qtr']==2]['month3_emplvl']   # jun
-            dfout.at[6,'TOTEMP'] = dfin[dfin['qtr']==3]['month1_emplvl']   # jul
-            dfout.at[7,'TOTEMP'] = dfin[dfin['qtr']==3]['month2_emplvl']   # aug
-            dfout.at[8,'TOTEMP'] = dfin[dfin['qtr']==3]['month3_emplvl']   # sep
-            dfout.at[9,'TOTEMP'] = dfin[dfin['qtr']==4]['month1_emplvl']   # oct
-            dfout.at[10,'TOTEMP']= dfin[dfin['qtr']==4]['month2_emplvl']   # nov
-            dfout.at[11,'TOTEMP']= dfin[dfin['qtr']==4]['month3_emplvl']   # dec
+            df = pd.DataFrame({'MONTH': months})
+            df['AVG_MONTHLY_EARNINGS'] = np.NaN
+            
+            # copy the earnings data into straight file and convert weekly to monthly
+            df.at[0,'AVG_MONTHLY_EARNINGS'] = dfin[dfin['qtr']==1]['avg_wkly_wage']   # jan
+            df.at[3,'AVG_MONTHLY_EARNINGS'] = dfin[dfin['qtr']==2]['avg_wkly_wage']   # mar
+            df.at[6,'AVG_MONTHLY_EARNINGS'] = dfin[dfin['qtr']==3]['avg_wkly_wage']   # jun
+            df.at[9,'AVG_MONTHLY_EARNINGS'] = dfin[dfin['qtr']==4]['avg_wkly_wage']   # oct            
+            df['AVG_MONTHLY_EARNINGS'] = df['AVG_MONTHLY_EARNINGS'] * (13.0 / 3.0)
+            
+            # for each industry, fill in the columns as appropriate
+            industry_equiv = [
+                ('TOTEMP',         '10'),                # Total, all industries
+                ('RETAIL_EMP',  '44-45'),                # Retail trade
+                ('EDHEALTH_EMP', '1025'),                # Education and health services
+                ('LEISURE_EMP',  '1026')                 # Leisure and hospitality    
+                ]                                        
+
+            for col, industry_code in industry_equiv:                
+                df[col] = 0
                 
-            # set a unique index
-            dfout.index = monthCount + pd.Series(range(0,len(dfout)))
-            monthCount += len(dfout)
+                # I need to add it up for the specific ownership titles
+                # own_code indicates type of government or private sector.  >0 is all (excluding sum of them all)
+                dfin = df_allrows[(df_allrows['own_code']>0) & (df_allrows['industry_code']==industry_code)]
                 
-            # append to the output store
-            outstore.append('countyEmp', dfout, data_columns=True)
+                # group across ownership categories
+                grouped = dfin.groupby('qtr')
+                agg = grouped.agg('sum')
+                
+                # fill in the actual column values
+                df.at[0,col] = agg.at[1,'month1_emplvl']   # jan
+                df.at[1,col] = agg.at[1,'month2_emplvl']   # feb
+                df.at[2,col] = agg.at[1,'month3_emplvl']   # mar
+                df.at[3,col] = agg.at[2,'month1_emplvl']   # apr
+                df.at[4,col] = agg.at[2,'month2_emplvl']   # may
+                df.at[5,col] = agg.at[2,'month3_emplvl']   # jun
+                df.at[6,col] = agg.at[3,'month1_emplvl']   # jul
+                df.at[7,col] = agg.at[3,'month2_emplvl']   # aug
+                df.at[8,col] = agg.at[3,'month3_emplvl']   # sep
+                df.at[9,col] = agg.at[4,'month1_emplvl']   # oct
+                df.at[10,col]= agg.at[4,'month2_emplvl']   # nov
+                df.at[11,col]= agg.at[4,'month3_emplvl']   # dec
+            
+            # calculate OTHER_EMP based on the difference from the total
+            df['OTHER_EMP'] = df['TOTEMP'] - df['RETAIL_EMP'] - df['EDHEALTH_EMP'] - df['LEISURE_EMP']
+            
+            # append to the full dataframe
+            dfout = dfout.append(df, ignore_index=True)
+            
+        # interpolate from quarterly to monthly values
+        dfout['AVG_MONTHLY_EARNINGS'] = dfout['AVG_MONTHLY_EARNINGS'].interpolate()
+            
+        # adjust for inflation
+        dfcpi  = self.getCPIFactors(cpiFile)
+        dfjoin = pd.merge(dfout, dfcpi, how='left', on=['MONTH'], sort=True)  
+        dfout['AVG_MONTHLY_EARNINGS_USD2010'] = dfjoin['AVG_MONTHLY_EARNINGS'] * dfjoin['CPI_FACTOR']
         
+        dfout.to_csv('c:/temp/qcew2.csv')
+        
+        # write the output
+        outstore.append('countyEmp', dfout, data_columns=True)        
         outstore.close()
         
 
@@ -115,9 +189,9 @@ class DemandHelper():
             outstore.remove('countyPop')
 
         # create the output file for annual data
-        annual = pd.DataFrame({'YEAR': range(2000, self.MAX_YEAR+1)})
+        annual = pd.DataFrame({'YEAR': range(self.POP_EST_YEARS[0], self.POP_EST_YEARS[1]+1)})
         annual['POP'] = 0
-        annual.index = range(2000, self.MAX_YEAR+1)
+        annual.index = range(self.POP_EST_YEARS[0], self.POP_EST_YEARS[1]+1)
         
         # get raw data, pre-2010, and copy to annual file
         pre2010_raw = pd.read_csv(pre2010File)
@@ -136,7 +210,7 @@ class DemandHelper():
         post2010_raw = post2010_raw[post2010_raw['Id2']==int(fips)]
         post2010_raw.index = range(0, len(post2010_raw))
         
-        for year in range(2010, self.MAX_YEAR+1): 
+        for year in range(2010, self.POP_EST_YEARS[1]+1): 
             annual.at[year,'POP'] = post2010_raw.at[0, 'Population Estimate (as of July 1) - ' + str(year)]
 
         
@@ -150,7 +224,7 @@ class DemandHelper():
                 
         monthly = pd.merge(monthly, annual, how='left', on=['MONTH'], sort=True)  
         monthly = monthly[['MONTH', 'POP']]
-        monthly = monthly.set_index(pd.DatetimeIndex(monthly['MONTH']))
+        monthly = monthly.set_index(pd.DatetimeIndex(monthly['MONTH']))     
         
         monthly = monthly.interpolate()
         
@@ -187,27 +261,11 @@ class DemandHelper():
         
         # normalize to the first day of the month
         df['MONTH'] = df['MONTH'].apply(pd.DateOffset(days=-14))
-        
-        # get the CPI and convert to monthly format
-        dfcpi = pd.read_excel(cpiFile, sheetname='BLS Data Series', skiprows=10, index_col=0)
-        base = dfcpi.at[2010, 'Annual']
-        
-        dfcpi = dfcpi.drop(['Annual', 'HALF1', 'HALF2'], axis=1)
-        dfcpi = dfcpi.stack()
-        dfcpi = dfcpi.reset_index()
-        dfcpi = dfcpi.rename(columns={
-                             'level_1' : 'monthString', 
-                             0 : 'CPI'
-                             })
-                             
-        dfcpi['MONTH'] = '01-' + dfcpi['monthString'] + '-' + dfcpi['Year'].astype('string')
-        dfcpi['MONTH'] = dfcpi['MONTH'].apply(pd.Timestamp)
-        
-        dfcpi['FACTOR'] = base / dfcpi['CPI']
-        
+                
         # adjust the fuel price for inflation
+        dfcpi = self.getCPIFactors(cpiFile)
         df = pd.merge(df, dfcpi, how='left', on=['MONTH'], sort=True)  
-        df['FUEL_PRICE_2010USD'] = df['FUEL_PRICE'] * df['FACTOR']
+        df['FUEL_PRICE_2010USD'] = df['FUEL_PRICE'] * df['CPI_FACTOR']
         
         # keep only the relevant columns
         df = df[['MONTH', 'FUEL_PRICE', 'FUEL_PRICE_2010USD', 'CPI']]        
@@ -216,4 +274,112 @@ class DemandHelper():
         outstore.append('fuelPrice', df, data_columns=True)
         outstore.close()
         
+        
+    def processLODESRAC(self, inputDir, xwalkFile, fips, outfile): 
+        '''
+        Processes data from the LODES (LEHD Origin-Destination Employment Statistics)
+        RAC (Resident Area Characteristics) files.  This gives us information
+        on workers at their home location.  Processed for SF county as a whole.
+        
+        inputDir - directory containing input CSV files
+        xwalkFile - file containing the geography crosswalk from LODES
+        fips - fips code for SF county
+        outfile - HDF file to write to
+        '''
+        
+        fips = int(fips)
+        
+        # remove the existing key so we don't overwrite
+        outstore = pd.HDFStore(outfile)
+        keys = outstore.keys()
+        if '/lodesRAC' in keys: 
+            outstore.remove('lodesRAC')
+            
+        # read the geography crosswalk
+        xwalk = pd.read_csv(xwalkFile)
+        xwalk['cty'] = xwalk['cty'].astype(int)
+        
+        # create the output file for annual data
+        years = range(self.LODES_YEARS[0], self.LODES_YEARS[1]+2)
+        annual = pd.DataFrame({'YEAR': years})
+        annual.index = years
+        
+        annual['WORKERS'] = np.NaN          # total workers
+        
+        annual['WORKERS_LOWINC'] = np.NaN  # Number of workers with earnings $1250/month or less
+        annual['WORKERS_MIDINC'] = np.NaN  # Number of workers with earnings $1251/month to $3333/month
+        annual['WORKERS_HIGHINC']= np.NaN  # Number of workers with earnings greater than $3333/month
+        
+        annual['WORKERS_RETAIL']   = np.NaN  # Number of workers in retail sector
+        annual['WORKERS_EDHEALTH'] = np.NaN  # Number of workers in education and health sector
+        annual['WORKERS_LEISURE']  = np.NaN  # Number of workers in leisure and hospitality sector
+        annual['WORKERS_OTHER']    = np.NaN  # Number of workers in other sectors
+        
+        
+        # get the data for each year
+        for year in years: 
+            
+            # read the data and aggregate to county level
+            infile = inputDir + '/ca_rac_S000_JT00_' + str(year) + '.csv' 
+            if os.path.isfile(infile):
+                    
+                print 'Reading LODES RAC data in ' + infile            
+                df = pd.read_csv(infile)            
+                df = pd.merge(df, xwalk, how='left', left_on='h_geocode', right_on='tabblk2010')            
+                df = df[df['cty']==fips]            
+                agg = df.groupby('cty').agg('sum')
+                
+                # copy over the appropriate fields
+                annual.at[year, 'WORKERS'] = agg.at[fips, 'C000']        
+                
+                annual.at[year, 'WORKERS_LOWINC'] = agg.at[fips, 'CE01']
+                annual.at[year, 'WORKERS_MIDINC'] = agg.at[fips, 'CE02'] 
+                annual.at[year, 'WORKERS_HIGHINC']= agg.at[fips, 'CE03'] 
+                
+                annual.at[year, 'WORKERS_RETAIL']   = agg.at[fips, 'CNS07'] 
+                annual.at[year, 'WORKERS_EDHEALTH'] = agg.at[fips, 'CNS15'] + agg.at[fips, 'CNS16'] 
+                annual.at[year, 'WORKERS_LEISURE']  = agg.at[fips, 'CNS17'] + agg.at[fips, 'CNS18'] 
+                annual.at[year, 'WORKERS_OTHER']    = (annual.at[year, 'WORKERS'] 
+                                                    -annual.at[year, 'WORKERS_RETAIL']
+                                                    -annual.at[year, 'WORKERS_EDHEALTH']
+                                                    -annual.at[year, 'WORKERS_LEISURE']
+                                                    )
+                                                    
+        # extrapolate the final year to get the last 6 months of data
+        extraYear = self.LODES_YEARS[1] + 1
+        for col in annual.columns:
+            annual.at[extraYear, col] =(annual.at[extraYear-1, col] + 
+                                       (annual.at[extraYear-1, col] 
+                                       -annual.at[extraYear-2, col]))
+                        
+        # expand to monthly, and interpolate values
+        annual['MONTH'] = annual['YEAR'].apply(lambda x: pd.Timestamp(str(x) + '-07-01'))
+        annual = annual.set_index(pd.DatetimeIndex(annual['MONTH']))
+        
+        monthly = annual[['MONTH']].resample('M')
+        monthly['MONTH'] = monthly.index
+        monthly['MONTH'] = monthly['MONTH'].apply(pd.DateOffset(days=1)).apply(pd.DateOffset(months=-1))
+                
+        monthly = pd.merge(monthly, annual, how='left', on=['MONTH'], sort=True)  
+        monthly = monthly.set_index(pd.DatetimeIndex(monthly['MONTH']))   
+        
+        monthly = monthly.interpolate()
+        
+        # drop the extraYear, which was just used to get to the end of the last year
+        monthly = monthly[monthly['YEAR']<extraYear-0.5]
+        monthly = monthly.drop('YEAR', 1)
+        
+        # convert to integers
+        for col in monthly.columns:  
+            if monthly[col].dtype == float: 
+                monthly[col] = monthly[col].astype(int)
+        
+        # set a unique index
+        monthly.index = pd.Series(range(0,len(monthly)))
+        
+        monthly.to_csv('c:/temp/rac.csv')
+        
+        # append to the output store
+        outstore.append('lodesRAC', monthly, data_columns=True)
+        outstore.close()
         
